9f6fee55183f2a314efcd2c1e7f3495ced87cc8c
YersonValenzuela12 <yerson_1232@hotmail.com>
2025-02-24 11:48:23 -0500

    priemr cambio

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './Button';
+
+function App() {
+  
+  return (
+  <div>
+  <h1>Un portal de bienvenida en react</h1>
+  <Button> Soy un boton</Button>
+    </div>
+  )
+
+}
+
+export default App
